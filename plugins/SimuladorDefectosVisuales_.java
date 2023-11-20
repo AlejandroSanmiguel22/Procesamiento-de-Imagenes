ecff766705b82bbcf1dcff249c6676fed0e64fa8
Alejandro Sanmiguel <alejandrosanmiguel0222@gmail.com>
2023-11-20 11:40:59 -0500

    Creación del metodo daltonismo

--- plugins/SimuladorDefectosVisuales_.java
+++ plugins/SimuladorDefectosVisuales_.java
@@ -22,55 +22,70 @@ public class SimuladorDefectosVisuales_ implements PlugInFilter {
     }
 
     @Override
-    public void run(ImageProcessor procesador) {
-        GenericDialog dialog = new GenericDialog("Simulador de Defectos Visuales");
-        String[] opciones = {"Miopía", "Hipermetropía", "Astigmatismo", "Cataratas", "Daltonismo"};
-        dialog.addChoice("Defecto visual:", opciones, opciones[0]);
-        dialog.addSlider("Severidad:", 0, 100, 50);
-        dialog.showDialog();
+public void run(ImageProcessor procesador) {
+    GenericDialog dialog = new GenericDialog("Simulador de Defectos Visuales");
+    String[] opciones = {"Miopía", "Hipermetropía", "Astigmatismo", "Cataratas", "Daltonismo"};
+    dialog.addChoice("Defecto visual:", opciones, opciones[0]);
+    dialog.addSlider("Severidad:", 0, 100, 50);
 
-        if (dialog.wasCanceled()) {
+    dialog.showDialog();
+
+    if (dialog.wasCanceled()) {
+        return;
+    }
+
+    String defectoSeleccionado = dialog.getNextChoice();
+    double severidad = dialog.getNextNumber();
+    double radioMaximo = Math.min(procesador.getWidth(), procesador.getHeight()) / 2.0 * (severidad / 100.0);
+
+    String tipoDaltonismo = "Deuteranopia"; // Valor predeterminado
+
+    // Si el usuario selecciona Daltonismo, muestra otro diálogo para elegir el tipo
+    if (defectoSeleccionado.equals("Daltonismo")) {
+        GenericDialog dialogDaltonismo = new GenericDialog("Tipo de Daltonismo");
+        String[] tiposDaltonismo = {"Deuteranopia", "Protanopia", "Tritanopia"};
+        dialogDaltonismo.addChoice("Tipo de Daltonismo:", tiposDaltonismo, tiposDaltonismo[0]);
+        dialogDaltonismo.showDialog();
+
+        if (dialogDaltonismo.wasCanceled()) {
             return;
         }
 
-        String defectoSeleccionado = dialog.getNextChoice();
-        double severidad = dialog.getNextNumber();
-        double radioMaximo = Math.min(procesador.getWidth(), procesador.getHeight()) / 2.0 * (severidad / 100.0);
-
-        // Aplica el efecto basado en la selección del usuario
-        switch (defectoSeleccionado) {
-            case "Miopía":
-                procesador = simularMiopia(procesador, severidad);
-                break;
-            case "Astigmatismo":
-                procesador = simularAstigmatismo(procesador, severidad);
-                break;
-            case "Hipermetropía":
-                procesador = simularHipermetropia(procesador, severidad, radioMaximo);
-                break;
-            case "Cataratas":
-                procesador = simularCataratas(procesador); // Asumiendo que tienes un método simularCataratas
-                break;
-            // Añade aquí los casos para otros defectos visuales
-            // ...
-            default:
-                IJ.log("Defecto visual no reconocido.");
-                return;
-        }
-
-        // Actualiza la imagen con el procesador modificado
-        ImagePlus imagenActual = new ImagePlus("Simulación de " + defectoSeleccionado, procesador);
-        imagenActual.show();
-
-        IJ.log("Defecto seleccionado: " + defectoSeleccionado);
-        IJ.log("Severidad: " + severidad);
+        tipoDaltonismo = dialogDaltonismo.getNextChoice();
     }
-    
-    
-    //****************************************************************************************//
 
-    
-     //Cataratas valor 50
+    // Aplica el efecto basado en la selección del usuario
+    switch (defectoSeleccionado) {
+        case "Miopía":
+            procesador = simularMiopia(procesador, severidad);
+            break;
+        case "Astigmatismo":
+            procesador = simularAstigmatismo(procesador, severidad);
+            break;
+        case "Hipermetropía":
+            procesador = simularHipermetropia(procesador, severidad, radioMaximo);
+            break;
+        case "Cataratas":
+            procesador = simularCataratas(procesador);
+            break;
+        case "Daltonismo":
+            procesador = simularDaltonismo(procesador, tipoDaltonismo);
+            break;
+        default:
+            IJ.log("Defecto visual no reconocido.");
+            return;
+    }
+
+    // Actualiza la imagen con el procesador modificado
+    ImagePlus imagenActual = new ImagePlus("Simulación de " + defectoSeleccionado, procesador);
+    imagenActual.show();
+
+    IJ.log("Defecto seleccionado: " + defectoSeleccionado);
+    IJ.log("Severidad: " + severidad);
+}
+
+    //****************************************************************************************//
+    //Cataratas valor 50
     private ImageProcessor simularCataratas(ImageProcessor original) {
         // Crear una copia del procesador de imagen original para no modificar la imagen original
         ImageProcessor copia = original.duplicate();
@@ -85,16 +100,46 @@ public class SimuladorDefectosVisuales_ implements PlugInFilter {
         // Ajustar el brillo si es necesario
         ImageStatistics stats = copia.getStatistics(); // Obtener estadísticas de la imagen
         copia.add(40); // Aumentar los valores de los píxeles para incrementar el brillo
-        
 
         // Devolver la imagen procesada
         return copia;
     }
-
-    
     //****************************************************************************************//
     
-    
+private ImageProcessor simularDaltonismo(ImageProcessor original, String tipoDaltonismo) {
+    int width = original.getWidth();
+    int height = original.getHeight();
+    ImageProcessor copia = original.duplicate();
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int pixel = original.getPixel(x, y);
+            int red = (pixel >> 16) & 0xff;
+            int green = (pixel >> 8) & 0xff;
+            int blue = pixel & 0xff;
+
+            switch (tipoDaltonismo) {
+                case "Deuteranopia": // Simulación de dificultad para percibir el verde
+                    green = (red + blue) / 2; // Ajustar el verde basado en el rojo y el azul
+                    break;
+                case "Protanopia": // Simulación de dificultad para percibir el rojo
+                    red = (green + blue) / 2; // Ajustar el rojo basado en el verde y el azul
+                    break;
+                case "Tritanopia": // Simulación de dificultad para percibir el azul
+                    blue = (red + green) / 2; // Ajustar el azul basado en el rojo y el verde
+                    break;
+                // Puedes agregar más casos para otros tipos de daltonismo
+            }
+
+            int newColor = (red << 16) | (green << 8) | blue;
+            copia.putPixel(x, y, newColor);
+        }
+    }
+
+    return copia;
+}
+
+    //****************************************************************************************//
     //Hipermetropía valor 12
     private ImageProcessor simularHipermetropia(ImageProcessor original, double severidad, double radioMaximo) {
         int width = original.getWidth();
